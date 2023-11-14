365cc96b3030c8f686785d91494410523e836b3e
Muhammadayub Erkinoff <muhammadayub2006@gmail.com>
2023-11-14 22:30:59 +0500

    xdvcsdv

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -1,6 +1,19 @@
 void problemSolution1(float consumed_water) {
-    float cost;
-    // write your code here
-
+    float cost=13;
+    if(consumed_water>=60) {
+        cost+=12+2.4+14;
+        cost+=(consumed_water-60)*1.5;
+    }
+    else if(consumed_water>=50) {
+        cost+=12+2.4;
+        cost+=(consumed_water-50)*1.4;
+    }
+    else if(consumed_water>=30) {
+        cost+=12;
+        cost+=(consumed_water-50)*0.12;
+    }
+    else{
+        cost+=(consumed_water-50)*0.4;
+    }
     return cost;
 }
--- problems/problem_4.cpp
+++ problems/problem_4.cpp
@@ -2,8 +2,22 @@
 #include <sstream>
 
 std::string problemSolution4(const std::string &macAddress) {
-    // write your code here
-
-    // make use of control flow statements
-    // return result;
+    bool b=1;
+    for(int i=0;i<17;i++){
+        if(macAddress[i]!='F'||macAddress[i]!=':'){
+            b=0;
+            break;
+        }
+    }
+    if(b){
+        return "Broadcast"l
+    }
+    else{
+        if(macAddress[1]=='0'||macAddress[1]=='2'||macAddress[1]=='4'||macAddress[1]=='6'||macAddress[1]=='8'||macAddress[1]=='A'||macAddress[1]=='C'||macAddress[1]=='E'){
+            return unicast;
+        }
+        else{
+            return multicast;
+        }
+    }
 }
